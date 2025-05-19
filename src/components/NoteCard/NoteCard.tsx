a542f386ef9c660353726cbf09b5ea7235e22c23
Dalia-Alawneh <alawnehdalia340@gmail.com>
2025-05-20 00:31:19 +0300

    Add props interface

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -1,44 +1,51 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material';
-import CustomIconButton from '../Shared/UI/IconButton/IconButton';
+import CustomIconButton from '../ui/IconButton/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import type { Theme } from '@mui/material/styles';
 
-export default function NoteCard() {
-  const theme = useTheme()
+const cardStyles = (theme: Theme) => ({
+  position: 'relative',
+  boxShadow: '0 0 8px 5px rgba(238, 238, 238, 0.29)',
+  backgroundColor: theme.noteColors.yellow,
+  p: '10px',
+  borderRadius: '12px',
+  '&:hover .hover-icon-btn': {
+    opacity: 1,
+  },
+});
+
+const iconButtonStyles = {
+  position: 'absolute',
+  top: 8,
+  right: 8,
+  opacity: 0,
+  transition: 'opacity 0.3s',
+  color: '#555',
+};
+
+interface INoteCardProps {
+  note: {
+    title: string;
+    content: string;
+  }
+}
+export default function NoteCard({ note }: INoteCardProps) {
 
   return (
-    <Card sx={{
-      position: 'relative',
-      maxWidth: 350,
-      boxShadow: '0 0 8px 5px rgba(238, 238, 238, 0.29)',
-      backgroundColor: theme.noteColors.yellow,
-      p: '10px',
-      borderRadius: '12px',
-      '&:hover .hover-icon-btn': {
-        opacity: 1,
-      },
-    }}>
+    <Card sx={(theme: Theme) => cardStyles(theme)}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {note.title}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {note.content}
         </Typography>
       </CardContent>
       <CustomIconButton aria-label="delete"
         className="hover-icon-btn"
-        sx={{
-          position: 'absolute',
-          top: 8,
-          right: 8,
-          opacity: 0,
-          transition: 'opacity 0.3s',
-          color: '#555',
-        }}>
+        sx={iconButtonStyles}>
         <DeleteIcon color='error' />
       </CustomIconButton>
     </Card>
