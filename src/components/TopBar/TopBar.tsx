c4c8a8b865ae066aa7ac4b6305b1cf884bd808c4
Dalia-Alawneh <alawnehdalia340@gmail.com>
2025-05-22 23:59:40 +0300

    test prettier hook

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -4,10 +4,9 @@ import type { ReactNode } from "react";
 const CustomAppBar = ({ children }: { children: ReactNode }) => {
   return (
     <AppBar color="secondary" sx={{ boxShadow: '0px 2px 15px -1px rgba(0, 0, 0, 0.2)' }}>
-      <Container maxWidth="xl" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        {children}
-      </Container>
-    </AppBar>
+      <Container maxWidth="xl" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>        {children}
+    </Container>
+  </AppBar>
   )
 }
 
