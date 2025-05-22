208f1ca1afa09a56b83dce7af264636ce0f59e94
Dalia-Alawneh <alawnehdalia340@gmail.com>
2025-05-23 00:02:23 +0300

    test prettier hook

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,14 +1,17 @@
-import { AppBar, Container } from "@mui/material"
-import type { ReactNode } from "react"
+import { AppBar, Container } from '@mui/material';
+import type { ReactNode } from 'react';
 
 const CustomAppBar = ({ children }: { children: ReactNode }) => {
   return (
-    <AppBar color="secondary" sx={{ boxShadow: '0px 2px 15px -1px rgba(0, 0, 0, 0.2)' }}>
-      <Container maxWidth="xl" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>       
-         {children}
+    <AppBar color='secondary' sx={{ boxShadow: '0px 2px 15px -1px rgba(0, 0, 0, 0.2)' }}>
+      <Container
+        maxWidth='xl'
+        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+      >
+        {children}
       </Container>
     </AppBar>
-  )
-}
+  );
+};
 
-export default CustomAppBar
+export default CustomAppBar;
