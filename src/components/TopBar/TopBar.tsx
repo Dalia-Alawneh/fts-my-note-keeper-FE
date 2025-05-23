95bfcb5fee8940badc4964ccdf0a5c08e7a1ac27
Dalia-Alawneh <alawnehdalia340@gmail.com>
2025-05-23 09:57:57 +0300

    Update container style

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,13 +1,19 @@
 import { AppBar, Container } from '@mui/material';
 import type { ReactNode } from 'react';
 
+const containerStyles = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexDirection: { xs: 'column', sm: 'row' },
+  gap: 1,
+  py: 2,
+};
+
 const CustomAppBar = ({ children }: { children: ReactNode }) => {
   return (
     <AppBar color='secondary' sx={{ boxShadow: '0px 2px 15px -1px rgba(0, 0, 0, 0.2)' }}>
-      <Container
-        maxWidth='xl'
-        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-      >
+      <Container maxWidth='xl' sx={containerStyles}>
         {children}
       </Container>
     </AppBar>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,37 +1,39 @@
-import { createTheme } from '@mui/material/styles';
-import './types'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import './types';
 
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#c5aefe',
-      "300": '#e6dff9',
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#c5aefe',
+        '300': '#e6dff9',
+      },
+      secondary: {
+        main: '#FFF',
+      },
+      text: {
+        primary: '#2D2E2F',
+        secondary: '#777B7E',
+      },
     },
-    secondary: {
-      main: '#FFF',
+    typography: {
+      fontFamily: 'Roboto, sans-serif',
+      h1: {
+        fontSize: '2.5rem',
+        fontWeight: 400,
+      },
     },
-    text: {
-      primary: '#2D2E2F',
-      secondary: '#777B7E',
+    noteColors: {
+      yellow: '#FFF59D',
+      pink: '#F48FB1',
+      green: '#A5D6A7',
+      blue: '#90CAF9',
+      purple: '#CE93D8',
+      orange: '#FFCC80',
+      grey: '#D3D3D3',
     },
-  },
-  typography: {
-    fontFamily: 'Roboto, sans-serif',
-    h1: {
-      fontSize: '2.5rem',
-      fontWeight: 400,
-    }
-  },
-  noteColors: {
-    yellow: '#FFF59D',
-    pink: '#F48FB1',
-    green: '#A5D6A7',
-    blue: '#90CAF9',
-    purple: '#CE93D8',
-    orange: '#FFCC80',
-    grey: '#D3D3D3'
-  }
-});
+  }),
+);
 
 export default theme;
